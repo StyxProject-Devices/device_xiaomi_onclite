b4c5676a755d02c7b14f177763d89f828b2d429b
KostyaJRZ <kostyajrzaws@gmail.com>
2021-10-16 00:40:10 +0300

    onclite: Switch to SELinux Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,8 @@ BOARD_KERNEL_CMDLINE := \
 	androidboot.bootdevice=7824900.sdhci \
 	androidboot.usbconfigfs=true \
 	loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_BOOTIMG_HEADER_VERSION := 1
